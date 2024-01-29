f50f7e645f4257466de9be6243ede8d32414c4a8
Niko Junnila <niko.junnila@goodlife.technology>
2024-01-29 13:58:22 +0200

    Convert remaining tab size 2's to 4's.

--- src/components/BubbleModal.vue
+++ src/components/BubbleModal.vue
@@ -2,21 +2,21 @@
 import { defineProps } from 'vue';
 
 defineProps<{
-  isModalActivate: Boolean,
+    isModalActivate: Boolean,
 }>()
 
 </script>
   
 <template>
-  <transition name="modal-animation">
-    <div v-show="isModalActivate" class="modal">
-      <transition name="modal-animation-inner">
-        <div class="modal-inner">
-          <slot />
+    <transition name="modal-animation">
+        <div v-show="isModalActivate" class="modal">
+            <transition name="modal-animation-inner">
+                <div class="modal-inner">
+                    <slot />
+                </div>
+            </transition>
         </div>
-      </transition>
-    </div>
-  </transition>
+    </transition>
 </template>
   
 <style lang="scss" scoped>
@@ -25,84 +25,84 @@ $secundary-color: var(--background-color);
 
 .modal-animation-enter-active,
 .modal-animation-leave-active {
-  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+    transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
 }
 
 .modal-animation-enter-from,
 .modal-animation-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 
 .modal-animation-inner-enter-active {
-  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
 }
 
 .modal-animation-inner-leave-active {
-  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
 }
 
 .modal-animation-inner-enter-from {
-  opacity: 0;
-  transform: scale(0.8);
+    opacity: 0;
+    transform: scale(0.8);
 }
 
 .modal-animation-inner-leave-to {
-  transform: scale(0.8);
+    transform: scale(0.8);
 }
 
 .modal {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: rgba(255, 255, 255, 0.5);
-  z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0.5);
+    z-index: 1;
 
-  .modal-inner {
-    position: relative;
-    max-width: 640px;
-    width: 80%;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-    background-color: #fff;
-    background-color: var(--panel-background-color);
-    padding: 2rem;
-    border-radius: 5px;
-    border: solid 1px gray;
+    .modal-inner {
+        position: relative;
+        max-width: 640px;
+        width: 80%;
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        background-color: #fff;
+        background-color: var(--panel-background-color);
+        padding: 2rem;
+        border-radius: 5px;
+        border: solid 1px gray;
 
-    i {
-      position: absolute;
-      top: 15px;
-      right: 15px;
-      font-size: 20px;
-      cursor: pointer;
+        i {
+            position: absolute;
+            top: 15px;
+            right: 15px;
+            font-size: 20px;
+            cursor: pointer;
 
-      &:hover {
-        color: crimson;
-      }
+            &:hover {
+                color: crimson;
+            }
+        }
+
+        button {
+            padding: 20px 30px;
+            border: none;
+            font-size: 16px;
+            background-color: crimson;
+            color: #fff;
+            cursor: pointer;
+        }
     }
-
-    button {
-      padding: 20px 30px;
-      border: none;
-      font-size: 16px;
-      background-color: crimson;
-      color: #fff;
-      cursor: pointer;
-    }
-  }
 }
 
 /* mobile*/
 @media screen and (max-width: 600px) {
-  .modal {
-    .modal-inner {
-      font-size: x-small;
-      padding: 1rem;
+    .modal {
+        .modal-inner {
+            font-size: x-small;
+            padding: 1rem;
+        }
     }
-  }
 }
 </style>
