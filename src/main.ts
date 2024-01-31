676ab4ff2fde86e127d4d72b9cc8e486307239b2
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-01-31 10:19:14 +0200

    imported Arrows spin from fontAwsome

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import { restoreSession } from './context/login'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret, faThumbsDown, faThumbsUp, faCopy, faTrash, faEllipsisVertical, faXmark, faSliders } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faThumbsDown, faThumbsUp, faCopy, faTrash, faEllipsisVertical, faXmark, faSliders, faArrowsSpin } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUserSecret, faThumbsDown, faThumbsUp, faCopy, faTrash, faEllipsisVertical, faXmark, faSliders)
+library.add(faUserSecret, faThumbsDown, faThumbsUp, faCopy, faTrash, faEllipsisVertical, faXmark, faSliders, faArrowsSpin)
 
 export const AppState = ref<"init" | "loaded" | "error">("init");
 
