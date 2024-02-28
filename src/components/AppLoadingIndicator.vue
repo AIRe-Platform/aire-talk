47e0c59ce74605ce55c80578a8d260b7aacd87cf
Niko Junnila <niko.junnila@goodlife.technology>
2024-02-28 11:21:13 +0200

    Fix app init loading indicator sizing.

--- src/components/AppLoadingIndicator.vue
+++ src/components/AppLoadingIndicator.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import { defineComponent } from "vue";
-defineComponent({ name: "AppLoadingIndicatorComponent " });
 </script>
 
 <template>
     <div class="app-loading-indicator">
-        <img
-            class="chatbot-icon"
-            src="@/assets/images/AIRe_loading-animaatio-test2_forever_750x750.gif"
-            alt="Logo"
-        />
+        <img class="chatbot-icon" src="@/assets/images/aire-splash-anim.gif" alt="Logo" />
     </div>
 </template>
 
 <style>
 .app-loading-indicator {
-    width: 56px;
-    min-height: 1rem;
-    transform: scale(0.15);
     margin: auto;
+
+    img {
+        width: 50vw;
+        height: 50vw;
+        max-width: 256px;
+        object-fit: contain;
+    }
 }
 </style>
