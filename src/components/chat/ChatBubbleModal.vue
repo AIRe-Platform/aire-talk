0fd0ee03437e73ab90a5f8e3630584f10139c2a3
Niko Junnila <niko.junnila@goodlife.technology>
2024-05-02 13:57:51 +0300

    Chat message modal is not behaving correctly. Disable its background to at least make it look ok.

--- src/components/chat/ChatBubbleModal.vue
+++ src/components/chat/ChatBubbleModal.vue
@@ -110,7 +110,6 @@ $secundary-color: var(--background-color);
     position: fixed;
     top: 0;
     left: 0;
-    background-color: rgba(255, 255, 255, 0.5);
     z-index: 9000;
 
     .modal-inner {
