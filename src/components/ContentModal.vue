f943f8ce59a80cbb762856bb599b05f83f1b90be
Niko Junnila <niko.junnila@goodlife.technology>
2024-05-16 13:12:58 +0300

    Revert type import 'fix', the issue was caused by something else.

--- src/components/ContentModal.vue
+++ src/components/ContentModal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
-import { Content, type ContentType } from "aire";
+import { Content, ContentType } from "aire";
 
 const props = defineProps<{
     active: boolean,
