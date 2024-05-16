79c7c263ceac469d619e91ad4520bd1ce130b487
Niko Junnila <niko.junnila@goodlife.technology>
2024-05-16 15:06:58 +0300

    Fix content catalogue Finnish translation

--- src/locales/fi.ts
+++ src/locales/fi.ts
@@ -48,7 +48,7 @@ const fi: Locale = {
     nav_theme: "Vaihda teema",
     nav_preferences: "Asetukset",
     nav_main_menu: "Päävalikko",
-    nav_catalogue: "Content Catalogue",
+    nav_catalogue: "Sisältökatalogi",
 
     profile_title: "Käyttäjätili",
     profile_label_first_name: "Etunimi",
