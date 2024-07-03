91fb719f815d6efdae3440beeb5fdca76d3e06b2
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-07-03 14:05:53 +0300

    Fixed typo.

--- src/components/profile/ProfileDeletionForm.vue
+++ src/components/profile/ProfileDeletionForm.vue
@@ -46,7 +46,7 @@ const onDeleteAccount = (e: Event) => {
 
 <template>
     <form id="delete-form" @submit.prevent="onDeleteAccount">
-        <h3>{{ $t(l.profile_heading_delete_accout) }}</h3>
+        <h3>{{ $t(l.profile_heading_delete_account) }}</h3>
         <div class="form-row description">{{ $t(l.profile_description_delete_account) }}</div>
         <div class="form-content">
             <span class="form-item">
--- src/locales/keys.ts
+++ src/locales/keys.ts
@@ -66,7 +66,7 @@ export enum LocalizationKey {
     profile_label_new_password = "profile_label_new_password",
     profile_button_change_password = "profile_button_change_password",
 
-    profile_heading_delete_accout = "profile_heading_delete_account",
+    profile_heading_delete_account = "profile_heading_delete_account",
     profile_description_delete_account = "profile_description_delete_account",
     profile_label_password_confirm = "profile_label_password_confirm",
     profile_label_keep_anonymized_data = "profile_label_keep_anonymized_data",
