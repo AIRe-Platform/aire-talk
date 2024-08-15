3d2be23271be7ddda318b2f0b11d9357bc52348d
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-08-15 16:08:17 +0300

    Adding MPL 2.0 license and also from its submodule.

--- src/helpers/chatMessages.ts
+++ src/helpers/chatMessages.ts
@@ -1,3 +1,8 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 import useLogin from "@/context/login";
 import i18n, { l } from "@/locales";
 import { ChatMessage } from "@/models/chat";
