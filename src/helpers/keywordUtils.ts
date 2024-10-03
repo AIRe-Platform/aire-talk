d2a42cc80a3d2afaee10d34724a03ffe09226ca3
Niko Junnila <niko.junnila@goodlife.technology>
2024-10-03 14:26:30 +0300

    Add MPL header to keywordUtils.ts

--- src/helpers/keywordUtils.ts
+++ src/helpers/keywordUtils.ts
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import { useKeywordCache } from "@/context/cache";
 import { AireKeyword, AireServices, AireStatus } from "aire";
 import { LanguageCode } from "iso-639-1";
