899e5aa4c40a512874ec2fc85c91b3dc30416f63
Tero Kilpeläinen <degenone1@gmail.com>
2024-11-25 14:55:17 +0200

    Add aria attribute to NavButton

--- src/components/layout/NavButton.vue
+++ src/components/layout/NavButton.vue
@@ -19,6 +19,7 @@ const props = defineProps<{
         class="nav-button"
         :class="{ 'nav-button-active': props.open }"
         :aria-expanded="props.open"
+        aria-haspopup="menu"
         :tabindex="UIState.reminderModalRef ? -1 : 0">
         <div>
             <Tooltip class="nav-button-graphics" :text="$t(l.nav_main_menu)" position="top" :useMaxContent="false"
