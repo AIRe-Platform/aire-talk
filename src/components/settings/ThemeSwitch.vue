a5c4e38f30dbd6a5034a085577683d4d5e5237f3
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-11-26 13:16:24 +0200

    Simplify the screen reader text for the switch UI teme

--- src/components/settings/ThemeSwitch.vue
+++ src/components/settings/ThemeSwitch.vue
@@ -20,9 +20,10 @@ const onSwitchTheme = (dark: boolean) => {
         <p class="theme-switch-label">{{ $t(l.switch_color_mode) }}</p>
         <Tooltip id="theme-tooltip" :text="$t(l.tooltip_menu_ui_mode)" position="top" :useMaxContent="false"
             :adjustPosition="true">
-            <div class="theme-switch" role="switch" :aria-checked="theme.style === 'theme-dark'"
-                :aria-label="`Switch between light or dark User Interface mode. Current mode is ${theme.style === 'theme-dark' ? 'dark mode' : 'light mode'}. Switch to ${theme.style !== 'theme-dark' ? 'dark mode' : 'light mode'}`"
-                tabindex="0" @keydown.prevent.space.enter="onSwitchTheme(theme.style !== 'theme-dark')"
+            <div class="theme-switch" role="switch" :aria-checked="theme.style === 'theme-dark'" :aria-label="theme.style === 'theme-dark'
+                ? 'Dark theme selected. Switch to light theme.'
+                : 'Light theme selected. Switch to dark theme.'" tabindex="0"
+                @keydown.prevent.space.enter="onSwitchTheme(theme.style !== 'theme-dark')"
                 @click="onSwitchTheme(theme.style !== 'theme-dark')">
                 <font-awesome-icon icon="fa-solid fa-sun" />
                 <Switch :is-on="theme.style === 'theme-dark'" />
