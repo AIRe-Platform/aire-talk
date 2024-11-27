bef1e93b93d133dd8d0c76af279d04d1773ca7dd
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-11-27 16:35:22 +0200

    Error message contrast ratio fixed.

--- src/components/chat/messages/ChatError.vue
+++ src/components/chat/messages/ChatError.vue
@@ -17,10 +17,10 @@ const props = defineProps<{
 <template>
     <div :id="props.message.id" class="chat-error" v-if="props.message.content">
         <h2 class="chat-error-title">
-            {{  $t(l.aire_system) }}
+            {{ $t(l.aire_system) }}
         </h2>
         <div class="chat-error-content">
-            {{  props.message.localize ?  $t(props.message.content) : props.message.content }}
+            {{ props.message.localize ? $t(props.message.content) : props.message.content }}
         </div>
     </div>
 </template>
@@ -31,9 +31,9 @@ const props = defineProps<{
     line-height: 1.4rem;
     padding: 0.5rem 1rem;
     margin: 1rem 1.5rem;
-    border: 2px solid var(--error-color);
+    border: 3px solid var(--error-color);
     background-color: var(--ia-chat-box-background);
-    color: var(--error-color);
+    color: var(--error-text-color);
     border-radius: 1rem;
     align-self: center;
     max-width: 80%;
