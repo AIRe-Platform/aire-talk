7b4c2c56c4293ec9ace644c79f728a4402df45bc
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-11-29 10:38:59 +0200

    NavButton element accessible for screen readers now communicate its functionality.

--- src/components/layout/NavMenu.vue
+++ src/components/layout/NavMenu.vue
@@ -110,7 +110,8 @@ onUnmounted(() => navMenuRef.value?.removeEventListener('focusout', focusOutList
 
 <template>
     <NavButton id="nav-burger-button" :data-tutorial-state="HomeTutorialState.Menu" :open="UIState.showMenu"
-        @click="onOpen"></NavButton>
+        @click="onOpen" :aria-expanded="UIState.showMenu" aria-controls="main-menu" aria-label="Toggle main menu">
+    </NavButton>
     <div class="nav-menu" ref="navMenuRef" :class="{
         'nav-menu-open': UIState.showMenu && !UIState.isClosingMenu, 'short-nav-menu': UIState.isNavMenuCompressed,
         'close-nav-menu-compressed-with-icons': UIState.isClosingMenu && UIState.isNavMenuCompressed && isIconsMenu,
