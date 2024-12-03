ad8a1e4f7c2288dc819b6dca8efc1978bf552df9
Tero Kilpeläinen <degenone1@gmail.com>
2024-12-03 12:57:17 +0200

    Fix ProfilePasswordForm error repeating on screen reader

--- src/components/profile/ProfilePasswordForm.vue
+++ src/components/profile/ProfilePasswordForm.vue
@@ -59,7 +59,7 @@ const onChangePassword = (e: Event) => {
                 <label class="form-label" for="new_password">{{ $t(l.profile_label_new_password) }}</label>
                 <input id="new_password" type="password" required="true" minlength="6" autocomplete="new-password"
                     v-model="state.newPassword" :readonly="state.busy"
-                    :aria-describedby="['current-password-description', state.error ? 'current-password-error' : undefined].filter(Boolean).join(' ')" />
+                    aria-describedby="current-password-description" />
             </span>
         </div>
         <div class="description" id="current-password-description" tabindex="0">{{ $t(l.profile_description_password) }}
