c2bcb4ffebe7126ee7433bf4aa08bac02c3231fc
Niko Junnila <niko.junnila@goodlife.technology>
2025-12-08 11:51:06 +0200

    Fallback to English translation or theme name if native translation is not defined.

--- src/components/chat/messages/ChatNotification.vue
+++ src/components/chat/messages/ChatNotification.vue
@@ -21,15 +21,20 @@ const keyword = computed(() => (props.message.theme || props.message.content)!)
 const content = computed(() => {
     const lang = getUILanguage();
     if (props.message.type == ChatMessageType.Keyword) {
-        return getKeywordTranslation(keyword.value, lang.value);
+        const native = getKeywordTranslation(keyword.value, lang.value);
+        if (native)
+            return native;
+
+        const english = getKeywordTranslation(keyword.value, "en");
+        if (english)
+            return english;
     }
     return keyword.value;
 });
 </script>
 
 <template>
-    <div :id="props.message.id" class="chat-notification"
-        v-if="props.message.type == ChatMessageType.Keyword && content">
+    <div :id="props.message.id" class="chat-notification" v-if="props.message.type == ChatMessageType.Keyword">
         {{ $t(l.notification_keyword, { keyword: content }) }}
         <Tooltip :text="$t(l.tooltip_remove_keyword)" position="top" :useMaxContent="false" :adjustPosition="true">
             <button class="button-keyword-delete" type="button" :aria-label=$t(l.tooltip_remove_keyword)
