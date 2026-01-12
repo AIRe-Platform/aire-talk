da223a92ea3cd1f8cd04cc1c3498e3452c3c4150
Niko Junnila <niko.junnila@goodlife.technology>
2026-01-12 10:43:10 +0200

    Misc. fixes and clean up
    * Fix reminder statistics events
    * Remove is_feedback_given conditional from chat end options
    * Clean up imports

--- src/helpers/agentUtils.ts
+++ src/helpers/agentUtils.ts
@@ -2,11 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import useChat from "@/context/chat";
 import { AireServices } from "aire";
 
 export function getAgentLocalized(name: string, lang: string): string | undefined {
-    const chat = useChat();
     if (AireServices.Agents) {
         const agent = AireServices.Agents.find(x => x.name === name);
         if (agent?.labels && agent.labels[lang] !== "")
