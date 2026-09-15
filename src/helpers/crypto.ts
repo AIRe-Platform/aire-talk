63a797af3ec79d1825478571b52065da6ae46012
Niko Junnila <niko.junnila@goodlife.technology>
2026-09-15 16:13:56 +0300

    Fix incorrect code challenge implementation, use stronger crypto on production builds

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -3,12 +3,17 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
-export function randomHexString(length: number) {
-    return [...Array(length)]
-        .map(() => Math.floor(Math.random() * 16)
-            .toString(16)
-        )
-        .join('');
+export function randomHexString(bytes: number) {
+    if (import.meta.env.VITE_AIRE_ENVIRONMENT === 'development') {
+        return [...Array(bytes)]
+            .map(() => Math.floor(Math.random() * 255).toString(16))
+            .join('');
+    }
+    else {
+        return [...crypto.getRandomValues(new Uint8Array(bytes))]
+            .map(x => x.toString(16))
+            .join('');
+    }
 }
 
 export async function SHA256(input: string) {
@@ -16,7 +21,7 @@ export async function SHA256(input: string) {
     const digest = await crypto.subtle.digest("SHA-256", buf);
     const data = new DataView(digest);
     let hash = "";
-    for(let i = 0; i < data.byteLength; i++)
+    for (let i = 0; i < data.byteLength; i++)
         hash += data.getUint8(i).toString(16).padStart(2, "0");
     return hash;
 }
